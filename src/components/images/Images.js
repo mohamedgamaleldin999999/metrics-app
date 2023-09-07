bd51caff6f778dfb1cf96fc38d500493db259e4e
mohamedgamaleldin999999 <123296170+mohamedgamaleldin999999@users.noreply.github.com>
2023-09-08 01:14:12 +0300

    Add imgs

---
+++ src/components/images/Images.js
@@ -0,0 +1,10 @@
+import img01 from './01.png';
+import img02 from './02.png';
+import img03 from './03.png';
+import img04 from './04.png';
+import img05 from './05.png';
+import img06 from './06.png';
+import img07 from './07.png';
+import img08 from './08.png';
+
+export default () => [img01, img02, img03, img04, img05, img06, img07, img08];
