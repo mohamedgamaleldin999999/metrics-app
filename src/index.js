b957e5d9b069c7b62da101f668d1f47d57a62eb7
mohamedgamaleldin999999 <123296170+mohamedgamaleldin999999@users.noreply.github.com>
2023-09-09 21:40:40 +0300

    fix linters

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './redux/configureStore';
-import './index.css';
+import './Index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <app />
+      <App />
     </Provider>
   </React.StrictMode>,
 );
