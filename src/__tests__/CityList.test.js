0b6f2b7664cdb6a0a7b79d2f14f5b5acd3d2e195
mohamedgamaleldin999999 <123296170+mohamedgamaleldin999999@users.noreply.github.com>
2023-09-10 04:00:39 +0300

    Fix all tests

--- src/__tests__/CityList.test.js
+++ src/__tests__/CityList.test.js
@@ -1,41 +1,36 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import Adapter from 'enzyme-adapter-react-16';
-import { shallow, configure } from 'enzyme';
-import configureStore from 'redux-mock-store';
-import CityList from '../components/CitiesList';
+import { render } from '@testing-library/react';
+import CitiesList from '../components/CitiesList';
 
-let store;
-const mockStore = configureStore([]);
-beforeEach(() => {
-  store = mockStore({
-    Zones: [
-      {
-        Zone: { lat: 0, lon: 0 },
-      },
-    ],
-  });
-});
-
-configure({ adapter: new Adapter() });
+// Mock the react-router-dom useNavigate function
 jest.mock('react-router-dom', () => ({
-  ...jest.requireActual('react-router-dom'),
-  useLocation: () => ({
-    zone: { lat: 0, lon: 0 },
-  }),
+  useNavigate: () => jest.fn(),
 }));
 
-jest.mock('axios', () => []);
+describe('CitiesList component', () => {
+  it('renders without errors', () => {
+    const zonesData = [
+      { name: 'City1', country: 'Country1', lat: 1, lon: 1 },
+      { name: 'City2', country: 'Country2', lat: 2, lon: 2 },
+    ];
 
-describe('<CityList Component />', () => {
-  it('should render City List Component', async () => {
-    const citylist = await shallow(
-      <Provider store={store}>
-        <CityList />
-      </Provider>,
-    );
-    process.nextTick(() => {
-      expect(citylist).toMatchSnapshot();
-    });
+    const { container } = render(<CitiesList zonesData={zonesData} />);
+    expect(container).toBeInTheDocument();
   });
+
+  it('renders cities correctly', () => {
+    const zonesData = [
+      { name: 'City1', country: 'Country1', lat: 1, lon: 1 },
+      { name: 'City2', country: 'Country2', lat: 2, lon: 2 },
+    ];
+
+    const { getByText } = render(<CitiesList zonesData={zonesData} />);
+
+    expect(getByText('City1')).toBeInTheDocument();
+    expect(getByText('Country1')).toBeInTheDocument();
+    expect(getByText('City2')).toBeInTheDocument();
+    expect(getByText('Country2')).toBeInTheDocument();
+  });
+
+  // Add more test cases as needed
 });
